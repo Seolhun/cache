8c30671e14ea92db0661a4c65d84ffa85d2e6fa3
Seolhun <shun10114@gmail.com>
2023-03-20 11:31:01 +0900

    refactor: update ordered set generic type name

--- src/OrderedSet/OrderedSet.ts
+++ src/OrderedSet/OrderedSet.ts
@@ -1,61 +1,61 @@
-export class OrderedSet<T> {
-  private set: Set<T>;
-  private arr: T[];
+export class OrderedSet<Item> {
+  private set: Set<Item>;
+  private items: Item[];
 
   constructor() {
-    this.set = new Set<T>();
-    this.arr = [];
-  }
-
-  add(value: T): this {
-    if (!this.set.has(value)) {
-      this.set.add(value);
-      this.arr.push(value);
-    }
-    return this;
-  }
-
-  delete(value: T): boolean {
-    const deleted = this.set.delete(value);
-    if (deleted) {
-      const index = this.arr.indexOf(value);
-      this.arr.splice(index, 1);
-    }
-    return deleted;
-  }
-
-  has(value: T): boolean {
-    return this.set.has(value);
-  }
-
-  clear(): void {
-    this.set.clear();
-    this.arr = [];
+    this.set = new Set<Item>();
+    this.items = [];
   }
 
   get size(): number {
     return this.set.size;
   }
 
-  values(): IterableIterator<T> {
+  add(value: Item): this {
+    if (!this.set.has(value)) {
+      this.set.add(value);
+      this.items.push(value);
+    }
+    return this;
+  }
+
+  delete(value: Item): boolean {
+    const deleted = this.set.delete(value);
+    if (deleted) {
+      const index = this.items.indexOf(value);
+      this.items.splice(index, 1);
+    }
+    return deleted;
+  }
+
+  has(value: Item): boolean {
+    return this.set.has(value);
+  }
+
+  clear(): void {
+    this.set.clear();
+    this.items = [];
+  }
+
+  values(): IterableIterator<Item> {
     return this.set.values();
   }
 
-  entries(): IterableIterator<[T, T]> {
+  entries(): IterableIterator<[Item, Item]> {
     return this.set.entries();
   }
 
-  [Symbol.iterator](): IterableIterator<T> {
+  [Symbol.iterator](): IterableIterator<Item> {
     return this.values();
   }
 
-  forEach(callback: (value: T, value2: T, set: Set<T>) => void, thisArg?: any): void {
+  forEach(callback: (value: Item, value2: Item, set: Set<Item>) => void, thisArg?: any): void {
     this.set.forEach((value, _, set) => {
       callback.call(thisArg, value, value, set);
     });
   }
 
-  toArray(): T[] {
-    return [...this.arr];
+  toArray(): Item[] {
+    return [...this.items];
   }
 }
